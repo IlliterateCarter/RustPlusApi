8fd3520424e57c1ba9d445155cbb0d23c26ed5c1
IlliterateCarter <rohrbachcarter@gmail.com>
2026-04-02 09:30:40 -0400

    Remove PatrolHelicopterMarkers from MapMarkers
    Removed PatrolHelicopterMarkers from MapMarkers class.

--- src/RustPlusApi/Data/MapMarkers.cs
+++ src/RustPlusApi/Data/MapMarkers.cs
@@ -12,5 +12,4 @@ public sealed record MapMarkers
     public Dictionary<uint, CargoShipMarker> CargoShipMarkers { get; init; } = [];
     // 6. Crates: doesn't appear anymore in the API
     // 7. GenericRadius: I don't know what is this
-    public Dictionary<uint, PatrolHelicopterMarker> PatrolHelicopterMarkers { get; init; } = [];
 }
